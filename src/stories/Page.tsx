0c9c7c45145d68e894ecb74f744e7a2f47238b24
Katie Poe Bayes <katpoe@gmail.com>
2025-07-21 18:19:02 -0400

    make a feature b change

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -47,6 +47,7 @@ export const Page: React.FC = () => {
             Use a higher-level connected component. Storybook helps you compose
             such data from the "args" of child component stories
           </li>
+          <li>Change on feature B</li>
           <li>
             Assemble data in the page component from your services. You can mock
             these services out using Storybook.
